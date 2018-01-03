7c264514895464ea28124809745b961de8c21cbb
Cour Jiang <jiangc4@CNADJIANGC4L1C.corp.emc.com>
2018-01-03 10:28:04 +0800

    11

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 Created on Feb 18, 2016
 @author: jh
 '''
-from flask_login import login_required
+from flask_login import login_required,current_user
 
 from flask import  render_template
 
 from appfactory import create_app
+import datetime 
 
 app = create_app()
 
@@ -17,7 +18,13 @@ app = create_app()
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.html')
+    now = datetime.datetime.now()
+    begin = datetime.datetime.strptime("20160725210504","%Y%m%d%H%M%S")
+    end = datetime.datetime.strptime("20491231232359","%Y%m%d%H%M%S")
+    total = (end-begin).days
+    used = (now-begin).days
+    msg = "%.1f%%  (%d/%d)"%(used*100.0/total,used,total)
+    return render_template('index.html',msg=msg)
 
 
 
