abc863b44467972fe5bafea8a406440ff0fdf1b8
courence <courence@163.com>
2018-02-12 09:51:11 +0800

    simplify

--- auth/auth.py
+++ auth/auth.py
@@ -11,57 +11,53 @@ from common.jsonresult import AjaxResult
 from flask_login import LoginManager, login_required, UserMixin, login_user, logout_user
 from model.usermodel import User
 
-
-ahthBlueprint = Blueprint('auth', __name__,
-                        template_folder='templates')
-
-
+ahthBlueprint = Blueprint('auth', __name__, template_folder='templates')
 
 
 @ahthBlueprint.route('/auth/signin', methods=['GET'])
 def signin():
     return render_template('auth/signin.html')
 
+
 @ahthBlueprint.route('/auth/signin', methods=['POST'])
 def validAndLogin():
     username = request.values.get('username')
     password = request.values.get('password')
-    if password and username :
+    if password and username:
         user = User.query.filter_by(username=username).first()
-        print user and user.password == password and login_user(user)
-        if user and user.validPassword(password) and login_user(user):
+        if user and user.validPassword(password) and login_user(
+                user, duration=datetime.timedelta(minutes=5)):
             return AjaxResult.successResult()
     return AjaxResult.failResult('登录名或密码错误')
+
 
 @ahthBlueprint.route('/auth/signup', methods=['GET'])
 def signup():
     return render_template('auth/signup.html')
 
-@ahthBlueprint.route('/auth/signup',methods=['POST'])
+
+@ahthBlueprint.route('/auth/signup', methods=['POST'])
 def register():
     '''注册'''
-    
+
     username = request.values.get('username')
     email = request.values.get('email')
     password = request.values.get('password')
-    
+
     if username and User.query.filter_by(username=username).first():
         return AjaxResult.failResult('该用户名已被使用')
     if email and User.query.filter_by(email=email).first():
         return AjaxResult.failResult('该用邮箱已被使用')
-    
-    user = User(username,password,email)
+
+    user = User(username, password, email)
     user.save()
     if login_user(user):
         return AjaxResult.successResult()
     else:
         return AjaxResult.failResult()
 
-    
-
 
 @ahthBlueprint.route('/auth/signout', methods=['GET'])
 def signout():
     logout_user()
     return redirect(url_for('auth.signin'))
-
--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,43 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 	//显示当前菜单
 	setActiveMenu();
 	//显示登录信息
 	showSignInfo();
-	
+
 	//显示登录信息
-	function showSignInfo(){
+	function showSignInfo() {
 		var url = '/auth/signin';
 		var menuName = 'SIGNIN'
-		if(window.is_signin){
+		if (window.is_signin) {
 			url = '/auth/signout';
 			menuName = 'SIGNOUT'
 		}
-		var html = '<li class="page-scroll"><a href="'+url+'">'+menuName+'</a></li>'
-		$(".nav").append(html);
+		var html = '<a href="' + url + '">' + menuName + '</a>'
+		$(".loginflag").html(html);
 	}
-	
-	$('.nav').find('a').on('click', function() {
+
+	$('.nav').find('a').on('click', function () {
 		var mainMenuName = $(this).html()
 		$.cookie('mainMenuName', mainMenuName, { path: '/' });
 	});
-	
+
 	//显示当前菜单
-	function setActiveMenu(){
+	function setActiveMenu() {
 		var mainMenuName = $.cookie('mainMenuName');
-		if(mainMenuName==null){
-			
+		if (mainMenuName == null) {
+
 		}
-		else{
-			$(".nav a").each(function(){
-			    if($(this).html()==mainMenuName){
-			    	$(this).parent('.page-scroll').addClass('active')
-			    }
-			  });
+		else {
+			$(".nav a").each(function () {
+				if ($(this).html() == mainMenuName) {
+					$(this).parent('.page-scroll').addClass('active')
+				}
+			});
 		}
-		
+
 	}
-	
-			
-});	    	
+
+
+});
 
